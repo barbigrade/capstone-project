0038c190a600b58c45094acb24dc83df6e4303cd
Barbara Balen <barbarabalen@barbaras-air.localdomain>
2022-09-21 23:45:43 +0200

    add h2 and a div that will display the list of bakeries

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,17 @@
+import styled from 'styled-components';
+import Head from 'next/head';
+
 export default function Home() {
   return (
-    <div>
-      <main></main>
-    </div>
+    <>
+      <Head>
+        <title>DAILY BREAD</title>
+      </Head>
+
+      <main>
+        <h2>All Bakeries</h2>
+        <div></div>
+      </main>
+    </>
   );
 }
