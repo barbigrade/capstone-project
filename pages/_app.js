155f0125e8964dac65663f8eb8da00cc40ffb73d
Barbara Balen <barbarabalen@barbaras-air.localdomain>
2022-09-21 14:29:32 +0200

    add GlobalStyle and Layout to the MyApp function

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
+import GlobalStyle from '../components/GlobalStyles';
+import Layout from '../components/Layout';
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <Layout>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
 
 export default MyApp;
